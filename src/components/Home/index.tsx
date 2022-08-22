873b6aac37e2e5f6e673e525989921f5656d5757
TNKKung <tomkabtokom@gmail.com>
2022-08-22 12:51:58 +0700

    Merge branch 'Brill/NavBarAndProfile' into develop

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const Home: React.FC = () => {
+    return(
+        <div className="flex items-center justify-center w-full h-full text-xl font-bold">
+          Nft marketPlace Homepage
+        </div>
+    )
+}
+export default Home;
