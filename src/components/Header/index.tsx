2ffbe00a6db159a910e9b757e019ef2b155e7243
TNKKung <tomkabtokom@gmail.com>
2022-09-05 11:51:28 +0700

    delete useAccount used store data in frontend

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,10 @@ import React, { useState, useEffect } from "react";
 
 import { useUserAccount } from "../../store/UserAction/hook";
 import { Link } from "react-router-dom";
-import useAccount from "../../hook/useAccount";
 
 const Header: React.FC = () => {
   const [connectBtnText, setConnectBtnText] = useState("Connect");
   const { address, loginMetamask } = useUserAccount();
-
-  const { createAccount } = useAccount();
 
   function isLogin() {
     if (address === undefined) {
@@ -18,7 +15,6 @@ const Header: React.FC = () => {
             className="nav-link btn btn-secondary text-white"
             onClick={() => {
               loginMetamask();
-              createAccount(address);
             }}
           >
             Connect Wallet
@@ -26,51 +22,55 @@ const Header: React.FC = () => {
         </li>
       );
     } else {
-      return (<div className="d-flex">
-        <li className="nav-item w-50 me-auto">
-          <div className="nav-link d-flex align-items-center">
-            <Link className="btn btn-secondary text-white ms-2" to="/createNFT">
-              Create
-            </Link>
-          </div>
-        </li>
-        <li className="nav-item d-flex align-items-center">
-          <div className="dropdown">
-            <button
-              className="btn btn-secondary dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              {connectBtnText}
-            </button>
-            <ul className="dropdown-menu dropdown-menu-end">
-              {/* <li><hr className="dropdown-divider" /></li> */}
-              <li>
-                <Link className="dropdown-item" to="/profile">
-                  Profile
-                </Link>
-              </li>
-              <li>
-                <Link className="dropdown-item" to="/setting">
-                  Setting
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="dropdown-item"
-                  to="/"
-                  onClick={() => {
-                    loginMetamask();
-                  }}
-                >
-                  Disconnect
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </li>
-      </div>
+      return (
+        <div className="d-flex">
+          <li className="nav-item w-50 me-auto">
+            <div className="nav-link d-flex align-items-center">
+              <Link
+                className="btn btn-secondary text-white ms-2"
+                to="/createNFT"
+              >
+                Create
+              </Link>
+            </div>
+          </li>
+          <li className="nav-item d-flex align-items-center">
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {connectBtnText}
+              </button>
+              <ul className="dropdown-menu dropdown-menu-end">
+                {/* <li><hr className="dropdown-divider" /></li> */}
+                <li>
+                  <Link className="dropdown-item" to="/profile">
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/setting">
+                    Setting
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="dropdown-item"
+                    to="/"
+                    onClick={() => {
+                      loginMetamask();
+                    }}
+                  >
+                    Disconnect
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </div>
       );
     }
   }
