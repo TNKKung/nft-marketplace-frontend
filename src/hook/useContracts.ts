8029ded9d1b821bc89ab30706a68f776bf719e70
TNKKung <tomkabtokom@gmail.com>
2022-09-07 02:34:31 +0700

    fix url for interacting with backend

--- src/hook/useContracts.ts
+++ src/hook/useContracts.ts
@@ -1,8 +1,8 @@
 import { ethers } from "ethers";
-
-import { CONTRACT_ADDRESS } from "../config/config";
-import contractABI from "../config/abi.json";
 import axios from "axios";
+
+import { CONTRACT_ADDRESS, baseUrl } from "../config";
+import contractABI from "../config/abi.json";
 
 const useContracts = (): any => {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -21,13 +21,11 @@ const useContracts = (): any => {
     let tokenId;
     try {
       await contract.mint(address, collaborator, collaboratorPercent, uri);
-      // "https://ipfs.pixura.io/ipfs/QmUyARmq5RUJk5zt7KUeaMLYB8SQbKHp3Gdqy5WSxRtPNa/SeaofRoses.jpg"
       tokenId = await contract.getTokenCurrent();
     } catch (error) {
       console.log(error);
     }
-    // console.log(Number(tokenCurrent) + 1);
-    const response = await axios.post("http://localhost:4000/createNFT", {
+    const response = await axios.post(`${baseUrl}/nft`, {
       owner: address,
       nameNFT,
       description,
