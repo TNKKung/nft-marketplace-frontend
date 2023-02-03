909a1cd1f49b33f3ec3215bd65ec02522be431f5
TNattawutK <tomkabtokom@gmail.com>
2023-02-04 03:02:03 +0700

    feat: response token from backend

--- src/hook/useAuth.ts
+++ src/hook/useAuth.ts
@@ -3,7 +3,7 @@ import Web3 from "web3";
 
 import { auth } from "../utils/firebase";
 import { baseUrl } from "../config";
-import { signInWithCustomToken, signOut } from "firebase/auth";
+import { signOut } from "firebase/auth";
 
 const useAuth = (): any => {
   const handleLogin = async (address: string): Promise<boolean> => {
@@ -14,8 +14,6 @@ const useAuth = (): any => {
 
       const messageToSign = response.data.response.messageToSign;
 
-      console.log(messageToSign);
-
       if (!messageToSign) {
         throw new Error("Invalid message to sign");
       }
@@ -24,24 +22,22 @@ const useAuth = (): any => {
       const signature = await web3.eth.personal.sign(
         messageToSign,
         address,
-        "111"
+        "123456"
       );
 
       const jwtResponse = await axios.get(
         `${baseUrl}/auth/jwt?address=${address}&signature=${signature}`
       );
 
-      console.log(jwtResponse);
-
-      const customToken = jwtResponse?.data?.response.customToken;
-
-      console.log(customToken);
-
-      if (!customToken) {
+      const [accessToken, refreshToken] = [
+        jwtResponse?.data?.response.accessToken,
+        jwtResponse?.data?.response.refreshToken,
+      ];
+      console.log({ accessToken, refreshToken });
+      if (!accessToken && !refreshToken) {
         throw new Error("Invalid JWT");
       }
-      const res = await signInWithCustomToken(auth, customToken);
-      console.log(res);
+
       return true;
     } catch (e) {
       return false;
