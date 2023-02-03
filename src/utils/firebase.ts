4524adf376a966fbef8ffbbee7a01aa82028ba53
TNattawutK <tomkabtokom@gmail.com>
2023-02-04 03:03:30 +0700

    fix: use process env

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCSOy_vCqXa_2isRLyzJpPwpI0Gn-jw2O4",
-  authDomain: "nft-marketplace-frontend.firebaseapp.com",
-  projectId: "nft-marketplace-frontend",
-  storageBucket: "nft-marketplace-frontend.appspot.com",
-  messagingSenderId: "1082818985782",
-  appId: "1:1082818985782:web:3f80d8f7813c258122fd97",
-  measurementId: "G-RLPP45HZ7W",
+  apiKey: process.env.REACT_APP_NFT_MARKETPLACE_APIKEY,
+  authDomain: process.env.REACT_APP_NFT_MARKETPLACE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_NFT_MARKETPLACE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_NFT_MARKETPLACE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_NFT_MARKETPLACE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_NFT_MARKETPLACE_APP_ID,
+  measurementId: process.env.REACT_APP_NFT_MARKETPLACE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
