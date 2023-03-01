7d0bc61170409112c1e791a26c8676c90f3823aa
TNattawutK <tomkabtokom@gmail.com>
2023-03-01 16:25:44 +0700

    fix: change api

--- src/config/index.js
+++ src/config/index.js
@@ -2,13 +2,13 @@ const CONTRACT_ADDRESS = "0x985F253fB2F1b47acAAA6fcdc1D00178f7E7B207";
 const Market_ADDRESS = "0xe4876D177a6aDf402fAD2af19a9EB057F462Ef28";
 
 // const baseUrl = "http://localhost:4000";
-const baseUrl = "http://localhost:5001/nft-marketplace-frontend/us-central1/app/v1"
 
 const blockchainName = "Sepolia";
 const chainID = "0xaa36a7";
 
 module.exports = {
-  baseUrl,
+  baseUrl:
+    "https://us-central1-nft-marketplace-frontend.cloudfunctions.net/app/v1",
   CONTRACT_ADDRESS,
   blockchainName,
   chainID,
