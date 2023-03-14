169f4949d66303f2d6ab016b3a16b84e91849932
TNattawutK <tomkabtokom@gmail.com>
2023-03-14 15:13:06 +0700

    fix: change export in file config

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,8 @@
-const CONTRACT_ADDRESS = "0x985F253fB2F1b47acAAA6fcdc1D00178f7E7B207";
-const Market_ADDRESS = "0xe4876D177a6aDf402fAD2af19a9EB057F462Ef28";
-
-// const baseUrl = "http://localhost:4000";
-
-const blockchainName = "Sepolia";
-const chainID = "0xaa36a7";
-
 module.exports = {
-  baseUrl:
-    "https://us-central1-nft-marketplace-frontend.cloudfunctions.net/app/v1",
-  CONTRACT_ADDRESS,
-  blockchainName,
-  chainID,
-  Market_ADDRESS,
+  baseUrl: process.env.REACT_APP_BACKEND_API_SERVICE,
+  CONTRACT_ADDRESS: "0x985F253fB2F1b47acAAA6fcdc1D00178f7E7B207",
+  Market_ADDRESS: "0xe4876D177a6aDf402fAD2af19a9EB057F462Ef28",
+  blockchainName: "Sepolia",
+  chainID: "0xaa36a7",
   jwtSecretKey: process.env.REACT_APP_JWT_SECRET_KEY,
 };
