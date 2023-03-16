167583f699011073941d3e5bb11760974f2ed45d
TNattawutK <tomkabtokom@gmail.com>
2023-03-17 02:00:11 +0700

    fix: format

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,19 @@
 import "./footer.css";
 
-const Footer:React.FC = () => {
-return(
+const Footer: React.FC = () => {
+  return (
     <div className="container">
-        <div className="container">
-            <div className="row align-items-center pt-4">
-                <h5 className="col-auto text-white mb-0">NFT Marketplace</h5>
-            </div>
-            <div className="row pb-4">
-                <div className="col-auto text-white">Digital marketplace for non-fugible tokens (NFT).</div>
-            </div>
+      <div className="container">
+        <div className="pt-4 row align-items-center">
+          <h5 className="col-auto mb-0 text-white">NFT Marketplace</h5>
         </div>
+        <div className="pb-4 row">
+          <div className="col-auto text-white">
+            Digital marketplace for non-fugible tokens (NFT).
+          </div>
+        </div>
+      </div>
     </div>
-)
-}
-export default Footer
+  );
+};
+export default Footer;
