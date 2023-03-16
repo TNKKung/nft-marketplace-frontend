84b9d4ee5523fb0bdcd9f6f75874020d8c3dcf01
TNattawutK <tomkabtokom@gmail.com>
2023-03-17 02:08:57 +0700

    fix: add props of exploreNFT

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import SettingPage from "./components/Setting";
@@ -10,7 +11,6 @@ import Collection from "./components/Collection";
 import SellNFT from "./components/SellNFT";
 import Search from "./components/Search";
 import ExploreNFT from "./components/ExploreNFT";
-import ExploreSaleNFT from "./components/ExploreSaleNFT";
 import ExploreCollection from "./components/ExploreCollection";
 
 function App() {
@@ -18,20 +18,32 @@ function App() {
     <div>
       <BrowserRouter>
         <Header />
-          <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/profile/:walletAddress" element={<Profile/>}></Route>
-            <Route path="/setting" element={<SettingPage/>}></Route>
-            <Route path="/createNFT" element={<CreateNFT/>}></Route>
-            <Route path="/myCollection" element={<MyCollection/>}></Route>
-            <Route path="/collection/:collectionId" element={<Collection/>}></Route>
-            <Route path="/viewNFT/:tokenID" element={<ViewNFT/>}></Route>
-            <Route path="/sellNFT/:tokenID" element={<SellNFT/>}></Route>
-            <Route path="/search/:searchValue" element={<Search/>}></Route>
-            <Route path="/viewNFT/" element={<ExploreNFT></ExploreNFT>}></Route>
-            <Route path="/viewSaleNFT/" element={<ExploreSaleNFT></ExploreSaleNFT>}></Route>
-            <Route path="/collection/" element={<ExploreCollection></ExploreCollection>}></Route>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/profile/:walletAddress" element={<Profile />}></Route>
+          <Route path="/setting" element={<SettingPage />}></Route>
+          <Route path="/createNFT" element={<CreateNFT />}></Route>
+          <Route path="/myCollection" element={<MyCollection />}></Route>
+          <Route
+            path="/collection/:collectionId"
+            element={<Collection />}
+          ></Route>
+          <Route path="/viewNFT/:tokenID" element={<ViewNFT />}></Route>
+          <Route path="/sellNFT/:tokenID" element={<SellNFT />}></Route>
+          <Route path="/search/:searchValue" element={<Search />}></Route>
+          <Route
+            path="/viewNFT/"
+            element={<ExploreNFT isSaleList={false}></ExploreNFT>}
+          ></Route>
+          <Route
+            path="/viewSaleNFT/"
+            element={<ExploreNFT isSaleList={true}></ExploreNFT>}
+          ></Route>
+          <Route
+            path="/collection/"
+            element={<ExploreCollection></ExploreCollection>}
+          ></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
