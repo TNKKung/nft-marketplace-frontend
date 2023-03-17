14ac4da372fc45d33a80dc7f10bc52b38f9db8f6
TNattawutK <tomkabtokom@gmail.com>
2023-03-17 11:58:50 +0700

    feat: add blank card in loading

--- src/components/ExploreCollection/index.tsx
+++ src/components/ExploreCollection/index.tsx
@@ -6,6 +6,7 @@ import Explore from "../Explore";
 import CollectionBox from "../boxComponent/CollectionBox";
 
 import useCollection from "../../hook/useCollection";
+import BlankCollectionCard from "../BlankCard/BlankCollectionCard";
 
 const ExploreCollection = () => {
   //const
@@ -14,54 +15,64 @@ const ExploreCollection = () => {
 
   //State
   const [showCollection, setShowCollection] = useState<CollectionObject[]>([]);
+  const [listCollection, setListCollection] = useState<CollectionObject[]>([]);
   const [filterNFT, setFilterNFT] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   //className State
-  const [ExploreCollection, setExplore] = useState<string>(displayNone);
 
   //hook
-  const { getAllCollection } = useCollection();
+  const { getAllCollection, getLengthCollection } = useCollection();
 
   //function
   const fetchData = useCallback(async () => {
-    const alltokenRes = await getAllCollection();
+    setLoading(true);
     try {
-      if (alltokenRes.length > 0) {
-        setShowCollection(alltokenRes);
-        setExplore(displayShow);
-      }
+      const alltokenRes = await getAllCollection();
+      setShowCollection(alltokenRes);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
+    setLoading(false);
   }, [getAllCollection]);
-
-  useEffect(() => {
-    if (showCollection.length > 0) {
-      setExplore(displayShow);
-    } else {
-      setExplore(displayNone);
-    }
-  }, [showCollection, filterNFT]);
 
   useEffect(() => {
     fetchData();
     // eslint-disable-next-line
   }, []);
 
+  useEffect(() => {
+    const fetchLengthCollection = async (): Promise<void> => {
+      try {
+        const lengthRes = await getLengthCollection();
+        setListCollection(lengthRes);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchLengthCollection();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Explore name="Collection" dropdown={false} setFilter={setFilterNFT}>
-      <div
-        className={"row mt-3 mb-5 justify-content-center" + ExploreCollection}
-      >
+      <div className={"row mt-3 mb-5 justify-content-center"}>
         <div
           className={
             "d-flex flex-row p-2 flex-wrap border border-secondary-subtle rounded home_show_list"
           }
         >
-          {showCollection.length !== 0 ? (
-            <>
+          {loading ? (
+            <div className="grid grid-cols-4 gap-3.5 animate-pulse w-full justify-items-center">
+              {listCollection.map(() => (
+                <BlankCollectionCard />
+              ))}
+            </div>
+          ) : (
+            <div className="grid grid-cols-4 gap-3.5 w-full justify-items-center">
               {showCollection.map((value: any) => (
-                <div className="m-2" key={value.collectionId}>
+                <div key={value.collectionId}>
                   <CollectionBox
                     owner={value.owner}
                     description={value.description}
@@ -73,14 +84,6 @@ const ExploreCollection = () => {
                   />
                 </div>
               ))}
-            </>
-          ) : (
-            <div
-              className={"container-fluid py-24 d-flex justify-content-center "}
-            >
-              <div className="spinner-border" role="status">
-                <span className="visually-hidden">Loading...</span>
-              </div>
             </div>
           )}
         </div>
