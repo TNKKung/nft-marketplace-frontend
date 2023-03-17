0f49276cfd5fae47ff5d0b7507361a356e9139ee
TNattawutK <tomkabtokom@gmail.com>
2023-03-17 11:53:24 +0700

    fix: use props

--- src/components/boxComponent/CollectionBox/index.tsx
+++ src/components/boxComponent/CollectionBox/index.tsx
@@ -13,46 +13,44 @@ const CollectionBox: React.FC<CollectionProps> = ({
   profileImage,
 }) => {
   return (
-    <div>
-      <div className="border rounded shadow-sm UserBox_box d-flex flex-column align-items-center">
-        <div className="border rounded shadow-sm container-fluid">
-          <div className="row">
-            <div className="p-0 col-12 placeholder-glow">
-              <img
-                src={nftImage ? nftImage : "/images/blank/blankBgImg.png"}
-                alt="bgProfileImage"
-                className={`collectionBox_bgImg bg-gray-300 ${
-                  nftImage ? "" : "placeholder"
-                }`}
-              ></img>
-            </div>
-          </div>
-
-          <div className="px-2 my-1 mt-2 row justify-content-end placeholder-glow">
-            <h6 className={"col-6 p-0 m-0 text-break text-truncate text-end"}>
-              {collectionName}
-            </h6>
-          </div>
-          <div className="px-2 mb-2 row justify-content-end placeholder-glow align-items-center">
-            <div className={"col-6 p-0 me-2 text-break text-truncate text-end"}>
-              {ownerName}
-            </div>
-            <div className="col-auto p-0">
-              <img
-                src={profileImage ? profileImage : "/images/blank/blankImg.png"}
-                alt="profileImage"
-                className={`collectionBox_profile p-0 bg-gray-800 ${
-                  profileImage ? "" : "placeholder"
-                }`}
-              ></img>
-            </div>
+    <div className="border rounded shadow-sm UserBox_box d-flex flex-column align-items-center">
+      <div className="border rounded shadow-sm container-fluid">
+        <div className="row">
+          <div className="p-0 col-12 placeholder-glow">
+            <img
+              src={nftImage ? nftImage : "/images/blank/blankBgImg.png"}
+              alt="bgProfileImage"
+              className={`collectionBox_bgImg bg-gray-300 ${
+                nftImage ? "" : "placeholder"
+              }`}
+            ></img>
           </div>
         </div>
-        <Link
-          className="top-0 position-absolute w-100 h-100"
-          to={"/collection/" + collectionId}
-        ></Link>
+
+        <div className="px-2 my-1 mt-2 row justify-content-end placeholder-glow">
+          <h6 className={"col-6 p-0 m-0 text-break text-truncate text-end"}>
+            {collectionName}
+          </h6>
+        </div>
+        <div className="px-2 mb-2 row justify-content-end placeholder-glow align-items-center">
+          <div className={"col-6 p-0 me-2 text-break text-truncate text-end"}>
+            {ownerName}
+          </div>
+          <div className="col-auto p-0">
+            <img
+              src={profileImage ? profileImage : "/images/blank/blankImg.png"}
+              alt="profileImage"
+              className={`collectionBox_profile p-0 bg-gray-800 ${
+                profileImage ? "" : "placeholder"
+              }`}
+            ></img>
+          </div>
+        </div>
       </div>
+      <Link
+        className="top-0 position-absolute w-100 h-100"
+        to={"/collection/" + collectionId}
+      ></Link>
     </div>
   );
 };
--- src/components/boxComponent/NFTBox/index.tsx
+++ src/components/boxComponent/NFTBox/index.tsx
@@ -1,128 +1,67 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import "./NFTBox.css";
 
-import useBackend from "../../../hook/useBackend";
-import useCollection from "../../../hook/useCollection";
 import etherPNG from "../../../asset/ethereum-icon.png";
 
 interface NFTProps {
-  TokenID: number;
+  tokenId: number;
+  URLImage: string;
+  collection: string;
+  NFTname: string;
+  saleNFTStatus: boolean;
+  price: string;
 }
-const NFTBox: React.FC<NFTProps> = ({ TokenID }) => {
-  const { readTokenIdData } = useBackend();
-  const { getCollectionbyId } = useCollection();
-
-  const [URLImage, setURLImage] = useState<string>(
-    "data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
-  );
-  const [loadingDataClass, setLoadingDataClass] =
-    useState<string>("placeholder");
-  const [NFTname, setNFTName] = useState<string>("NFT Name");
-  const [saleNFTStatus, setSaleNFTStatus] = useState<boolean>(false);
-  const [nftCost, setNFTCost] = useState<number>(0);
-  const [collection, setCollection] = useState<string>("Collection Name");
-
-  const fetchData = useCallback(async () => {
-    const DataDetail = await readTokenIdData(TokenID);
-    if (DataDetail.statusSale === true) {
-      setSaleNFTStatus(true);
-      try {
-        setNFTCost(DataDetail.price);
-      } catch (Error) {
-        console.log(Error);
-      }
-    } else {
-      setSaleNFTStatus(false);
-    }
-    try {
-      // const TokenURI = await readTokenURI(TokenID);
-      setURLImage(DataDetail.tokenURI);
-      setNFTName(DataDetail.nameNFT);
-      setLoadingDataClass("");
-      if (
-        DataDetail.collectionId !== "" &&
-        DataDetail.collectionId !== "none"
-      ) {
-        try {
-          const collectionRes = await getCollectionbyId(
-            DataDetail.collectionId
-          );
-          setCollection(collectionRes.collectionName);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    } catch (error) {
-      setNFTName("NFT Name");
-    }
-  }, [
-    TokenID,
-    // readOwnerTokenID,
-    readTokenIdData,
-    // readTokenURI,
-    getCollectionbyId,
-  ]);
-
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+const NFTBox: React.FC<NFTProps> = ({
+  tokenId,
+  NFTname,
+  URLImage,
+  collection,
+  price,
+  saleNFTStatus,
+}) => {
   return (
-    <div>
-      <div className="m-2 border rounded shadow-sm NFTBox_box d-flex flex-column align-items-center placeholder-glow">
-        <img
-          className={"NFTBox_previewImg " + loadingDataClass}
-          src={URLImage}
-          alt="previewImage"
-          loading="lazy"
-        ></img>
-        <div className="flex flex-row p-0 px-2 m-0 justify-content-between align-items-center w-100 NFTBox_textBox">
-          <div className="placeholder-glow NFTBox_text">
-            {collection === "Collection Name" ? null : (
+    <div className="m-2 border rounded shadow-sm NFTBox_box d-flex flex-column align-items-center placeholder-glow">
+      <img
+        className={"NFTBox_previewImg "}
+        src={URLImage}
+        alt="previewImage"
+        loading="lazy"
+      ></img>
+      <div className="flex flex-row p-0 px-2 m-0 justify-content-between align-items-center w-100 NFTBox_textBox">
+        <div className="placeholder-glow NFTBox_text">
+          {collection === "Collection Name" ? null : (
+            <h6 className={"p-0 m-0 w-100 text-break text-truncate "}>
+              {collection}
+            </h6>
+          )}
+          <p className={"p-0 m-0 w-100 text-break text-truncate "}>{NFTname}</p>
+        </div>
+        {saleNFTStatus === false ? null : (
+          <div className="placeholder-glow">
+            <div className="flex flex-row align-items-center">
+              <img
+                className="NFTBox_etherIcon"
+                src={etherPNG}
+                alt="etherIcon"
+              />
               <h6
                 className={
-                  "p-0 m-0 w-100 text-break text-truncate " + loadingDataClass
+                  "p-0 m-0 w-100 text-break text-truncate text-end justify-content-center"
                 }
               >
-                {collection}
+                {price}
               </h6>
-            )}
-            <p
-              className={
-                "p-0 m-0 w-100 text-break text-truncate " + loadingDataClass
-              }
-            >
-              {NFTname}
-            </p>
-          </div>
-          {saleNFTStatus === false ? null : (
-            <div className="placeholder-glow">
-              <div className="flex flex-row align-items-center">
-                <img
-                  className="NFTBox_etherIcon"
-                  src={etherPNG}
-                  alt="etherIcon"
-                />
-                <h6
-                  className={
-                    "p-0 m-0 w-100 text-break text-truncate text-end justify-content-center" +
-                    loadingDataClass
-                  }
-                >
-                  {nftCost}
-                </h6>
-              </div>
             </div>
-          )}
-        </div>
-
-        <Link
-          className="top-0 position-absolute w-100 h-100"
-          to={"/viewNFT/" + TokenID}
-        ></Link>
+          </div>
+        )}
       </div>
+
+      <Link
+        className="top-0 position-absolute w-100 h-100"
+        to={"/viewNFT/" + tokenId}
+      ></Link>
     </div>
   );
 };
